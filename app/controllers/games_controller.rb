768e3e1f8e58175d6952ecda931d815ec8bdfbe9
Lucy Stringer <lucyjstringer@gmail.com>
2021-11-06 18:25:30 +0000

    add games controller & initial pass at methods in that controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,50 @@
+class GamesController < ApplicationController
+  before_action :find_game, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @games = Game.all
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+
+  def create
+    # amend fields for game - check questionnaire
+    @game = Game.new(user_id: session[:user_id], arrival: params[:booking][:arrival], departure: params[:booking][:departure])
+    if @game.save
+      redirect_to games_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @game.update(game_params)
+      redirect_to games_url
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @game.destroy
+    redirect_to games_url
+  end
+
+  private
+
+  def find_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    # check game params & update
+    params.require(:booking).permit(:arrival, :departure)
+  end
+end
