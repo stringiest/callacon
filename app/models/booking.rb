c75bd0ade49c2adc3c9bc81390216bf63692ab2c
Lucy Stringer <lucyjstringer@gmail.com>
2022-02-20 10:59:46 +0000

    add sps, assistance & donation elements to cost calculation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,13 +4,7 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   before_save do |booking|
-    puts "booking"
-    puts booking
-    puts "in the after_create method"
     cost = CostCalculator.new(booking)
-    puts "***********************************"
-    puts "cost.calculate"
-    puts cost.calculate
     self.cost = cost.calculate
   end
 end
--- lib/cost_calculator.rb
+++ lib/cost_calculator.rb
@@ -1,17 +1,38 @@
 class CostCalculator
+  # store all currency amounts in pence in the database
   def initialize(booking)
     @booking = booking
   end
 
   def calculate
-    # £50 initial fee, plus £35 per night for Mon - Sat night, £20 per night for Sun
-    # straight subtraction doesn't return integer days in current format
-    # how do I return integer days?
-    if @booking.departure.strftime("%w") == 6
-      total = (50 + 35 * (@booking.departure - @booking.arrival).to_i + 20) * 100
+    if @booking.departure.strftime("%u") == "1"
+      total = ((50 + (35 * (number_of_nights - 1)) + 20 + single_person_supplement) / assistance_claim  + donation) * 100
     else
-      total = (50 + 35 * ((@booking.departure - @booking.arrival).to_i + 1)) * 100
+      total = ((50 + (35 * number_of_nights) + single_person_supplement) / assistance_claim + donation) * 100
     end
     total
+  end
+
+  private
+
+  def number_of_nights
+    (@booking.departure - @booking.arrival).to_i
+  end
+
+  def assistance_claim
+    @booking.assistance == TRUE ? 2 : 1
+  end
+
+  def donation
+    @booking.donation
+  end
+
+  def single_person_supplement
+    # £50 per night, capped at £200 (i.e. price stops increasing after 4 nights)
+    if @booking.sps == true
+      number_of_nights > 3 ? 200 : number_of_nights * 50
+    else
+      0
+    end
   end
 end
