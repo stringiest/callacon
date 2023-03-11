5ba858328149c67e4f00b45b6318ca4dd944a668
Lucy Stringer <lucyjstringer@gmail.com>
2023-03-11 16:18:25 +0000

    add redirect to new session if not logged in

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @bookings = Booking.where(["user_id = #{session[:user_id]}"])
-    @games = Game.where(["user_id = #{session[:user_id]}"])
+    if session[:user_id] != nil
+      @bookings = Booking.where(["user_id = #{session[:user_id]}"])
+      @games = Game.where(["user_id = #{session[:user_id]}"])
+    else
+      redirect_to new_session_path
+    end
   end
 end
